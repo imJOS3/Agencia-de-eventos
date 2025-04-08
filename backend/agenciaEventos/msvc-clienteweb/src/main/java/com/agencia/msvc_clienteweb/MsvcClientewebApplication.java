facae95b438a8f8fdcedd624ea5171a3a374491e
jose <josebenjumea2005@gmail.com>
2025-04-08 17:00:46 -0500

    backend base

---
+++ backend/agenciaEventos/msvc-clienteweb/src/main/java/com/agencia/msvc_clienteweb/MsvcClientewebApplication.java
@@ -0,0 +1,13 @@
+package com.agencia.msvc_clienteweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcClientewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcClientewebApplication.class, args);
+	}
+
+}
---
+++ backend/agenciaEventos/msvc-clienteweb/src/test/java/com/agencia/msvc_clienteweb/MsvcClientewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agencia.msvc_clienteweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcClientewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
